1d7e70ceded544e6e4bc42d5958f9eb8eb0df681
Rostislav Kozar <argulusyn@gmail.com>
2019-01-25 22:44:21 +0200

    Changed to secure protocol
    Changed to secure protocol

--- src/services/ShowsService.js
+++ src/services/ShowsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://api.tvmaze.com";
+const API = "https://api.tvmaze.com";
 const searchQuery = "/search/shows?q=";
 
 export function getShowsByQuery(query) {
