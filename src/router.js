18d73cc44657ccd4bbf7f2c6bb0452ee8ce74908
Rostislav Kozar <argulusyn@gmail.com>
2019-01-25 17:10:32 +0200

    Add new route
    Add new route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Search from "./views/Search.vue";
+import ShowDetailed from "./views/ShowDetailed";
 
 Vue.use(Router);
 
@@ -13,12 +14,17 @@ export default new Router({
     },
     {
       path: "/favorites",
-      name: "favorites",
+      name: "Favorites",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/Favorites.vue")
+    },
+    {
+      path: "/details/:index",
+      name: "Details",
+      component: ShowDetailed
     }
   ]
 });
