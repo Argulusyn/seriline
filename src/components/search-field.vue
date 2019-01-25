8fb7f2e22d932b0eb19f6b5f59f6d38c4dfae8ee
Rostislav Kozar <argulusyn@gmail.com>
2019-01-25 09:50:48 +0200

    Deleted template
    Deleted template

---
+++ src/components/search-field.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: "SearchField",
+  props: {
+    msg: String
+  }
+};
+</script>
---
+++ src/views/Search.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+// @ is an alias to /src
+import SearchField from "@/components/search-field.vue";
+
+export default {
+  name: "Search",
+  components: {
+    SearchField
+  }
+};
+</script>
