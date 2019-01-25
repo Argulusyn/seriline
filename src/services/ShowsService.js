abb1b8c19dd508a18efa5084dab7fd5f2c069741
Rostislav Kozar <argulusyn@gmail.com>
2019-01-25 15:07:36 +0200

    Added shows parsing
    Added shows parsing after getting from API

--- src/services/ShowsService.js
+++ src/services/ShowsService.js
@@ -4,5 +4,17 @@ const API = "http://api.tvmaze.com";
 const searchQuery = "/search/shows?q=";
 
 export function getShowsByQuery(query) {
-  return axios.get(API + searchQuery + query);
+  return new Promise(resolve => {
+    axios.get(API + searchQuery + query).then(data => {
+      resolve(getShowsFromResponseData(data));
+    });
+  });
+}
+
+function getShowsFromResponseData(responseData) {
+  const shows = [];
+  responseData.data.map(element => {
+    shows.push(element.show);
+  });
+  return shows;
 }
