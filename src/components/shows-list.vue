7b025fd5100688690ac27bc44fbf15292c2bae12
Rostislav Kozar <argulusyn@gmail.com>
2019-01-25 15:07:03 +0200

    Added show-list component
    Added show-list component

---
+++ src/components/shows-list.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list-container">
+        <div class="columns">
+            <show-card class="column is-one-quarter" v-for="show in shows" :show="show" :key="show.id"></show-card>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ShowCard from "./show-card";
+
+    export default {
+        name: "ShowsList",
+        components: {
+            ShowCard
+        },
+        props: {
+            shows: null
+        }
+    };
+</script>
+
+<style scoped>
+    .list-container {
+        margin: 20px;
+    }
+    .columns {
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    .column {
+        margin: 10px;
+        border-radius: 10px;
+    }
+</style>
